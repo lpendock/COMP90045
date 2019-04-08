5d5a1366426f67acdb6d26564a34a6212b797878
lpendock <lpendock@student.unimelb.edu.au>
2019-04-08 21:26:47 +1000

    Added some initial extensions to KidParser

---
+++ GoatAST.hs
@@ -0,0 +1,57 @@
+module GoatAST where
+
+-----------------------------------
+-- Specification of an AST for Goat 
+-----------------------------------
+
+type Ident = String
+
+data BaseType
+    = BoolType | IntType | FloatType
+    deriving (Show, Eq)
+
+data Lvalue
+    = LId Ident
+    deriving (Show, Eq)
+
+data Binop
+    = Op_add | Op_mul | Op_sub | Op_div | Op_or | Op_and | Op_equal | Op_not_equal 
+    | Op_less | Op_lequal | Op_greater | Op_grequal
+    deriving (Show, Eq)
+
+data Expr
+    = BoolConst Bool
+    | IntConst Int
+    | StrConst String
+    | Id Ident
+    | Add Expr Expr
+    | Sub Expr Expr
+    | Mul Expr Expr
+    | Div Expr Expr
+    | Eq Expr Expr
+    | Neq Expr Expr
+    | Greater Expr Expr
+    | Greq Expr Expr
+    | Less Expr Expr
+    | Lequal Expr Expr
+    | UnaryMinus Expr
+    | Not Expr
+    deriving (Show, Eq)
+
+data Decl
+    = Decl Ident BaseType
+    deriving (Show, Eq)
+
+data Stmt
+    = Assign Lvalue Expr
+    | Read Lvalue
+    | Write Expr
+    | Call Ident [Expr]
+    | If Expr [Stmt]
+    | IfElse Expr [Stmt] [Stmt]
+    | While Expr [Stmt]
+    deriving (Show, Eq)
+
+data GoatProgram
+    = Program [Decl] [Stmt]
+    deriving (Show, Eq)
---
+++ GoatParser.hs
@@ -0,0 +1,277 @@
+module Main where
+
+import GoatAST
+import Data.Char
+import Text.Parsec
+import Text.Parsec.Language (emptyDef)
+import qualified Text.Parsec.Token as Q
+import System.Environment
+import System.Exit
+
+type Parser a
+    = Parsec String Int a
+
+lexer :: Q.TokenParser Int
+lexer
+    = Q.makeTokenParser
+        (emptyDef
+        { Q.commentLine     = "#"
+        , Q.nestedComments  = False
+        , Q.identStart      = letter
+        , Q.identLetter     = alphaNum <|> char '_'
+        , Q.opStart         = oneOf "|&!=<>+-*/:"
+        , Q.reservedNames   = myReserved
+        , Q.reservedOpNames = myOpnames
+        })
+
+whiteSpace = Q.whiteSpace lexer
+lexeme     = Q.lexeme lexer
+natural    = Q.natural lexer
+identifier = Q.identifier lexer
+colon      = Q.colon lexer
+semi       = Q.semi lexer
+comma      = Q.comma lexer
+parens     = Q.parens lexer
+squares    = Q.squares lexer
+reserved   = Q.reserved lexer
+reservedOp = Q.reservedOp lexer
+
+myReserved, myOpnames :: [String]
+
+myReserved = 
+    ["begin", "bool", "call", "do", "else", "end", "false", "fi", "float", "if", "int", "od", "proc", "read", "ref", "then", "true", "val", "while", "write"]
+
+myOpnames 
+    = ["||", "&&", "!", "=", "!=", "<", "<=", ">", ">=", "+", "-", "*", "/", ":="]
+
+-----------------------------------------------------------------
+--  pProg is the topmost parsing function. It looks for a program
+--  header "proc main()", followed by the program body.
+-----------------------------------------------------------------
+
+pProg :: Parser GoatProgram
+pProg
+    = do
+        reserved "proc"
+        reserved "main"
+        parens (return ())
+        (decls,stmts) <- pProgBody
+        return (Program decls stmts)
+
+-----------------------------------------------------------------
+--  pProgBody looks for a sequence of declarations followed by a
+--  sequence of statements.
+-----------------------------------------------------------------
+
+pProgBody :: Parser ([Decl],[Stmt])
+pProgBody
+    = do
+        decls <- many pDecl
+        reserved "begin"
+        stmts <- many1 pStmt
+        reserved "end"
+        return (decls,stmts)
+
+pDecl :: Parser Decl
+pDecl
+    = do
+        basetype <- pBaseType
+        ident <- identifier
+        whiteSpace
+        semi
+        return (Decl ident basetype)
+
+pBaseType :: Parser BaseType
+pBaseType
+    = do { reserved "bool"; return BoolType }
+        <|>
+        do { reserved "int"; return IntType }
+        <|>
+        do { reserved "float"; return FloatType}
+
+-----------------------------------------------------------------
+--  pStmt is the main parser for statements. It wants to recognise
+--  read and write statements, and assignments.
+-----------------------------------------------------------------
+
+pStmt, pRead, pWrite, pAsg, pCall :: Parser Stmt
+
+pStmt 
+    = choice [pRead, pWrite, pAsg, pCall]
+
+pRead
+    = do 
+        reserved "read"
+        lvalue <- pLvalue
+        semi
+        return (Read lvalue)
+
+pWrite
+    = do 
+        reserved "write"
+        exp <- (pString <|> pExp)
+        semi
+        return (Write exp)
+
+pAsg
+    = do
+        lvalue <- pLvalue
+        reservedOp ":="
+        rvalue <- pExp
+        semi
+        return (Assign lvalue rvalue)
+
+pCall
+    = do 
+        reserved "call"
+        exp <- (pString <|> pExp)
+        semi
+        return (Call exp)
+
+-----------------------------------------------------------------
+--  pExp is the main parser for expressions. It takes into account
+--  the operator precedences and the fact that the binary operators
+--  are left-associative.
+-----------------------------------------------------------------
+
+pExp, pTerm, pFactor, pUminus, pNum, pIdent, pString :: Parser Expr
+
+pExp 
+  = pString <|> (chainl1 pTerm pAddOp)
+    <?>
+    "expression"
+
+pString 
+  = do
+      char '"'
+      str <- many (satisfy (/= '"'))
+      char '"'
+      return (StrConst str)
+    <?>
+    "string"
+
+pAddOp, pMulOp, pSubOp, pDivOp :: Parser (Expr -> Expr -> Expr)
+pEqOp, pNeqOp, pLeqOp, pGeqOp :: Parser (Expr -> Expr -> Expr)
+pGreOP, pLesOp :: Parser (Expr -> Expr -> Expr)
+
+pAddOp
+  = do 
+      reservedOp "+"
+      return Add
+
+pDivOp
+    = do
+        reservedOp "/"
+        return Div
+
+pSubOp
+    = do
+        reservedOp "-"
+        return Sub
+
+pEqOp
+    = do
+        reservedOp "="
+        return Eq
+
+pNeqOp
+    = do
+        reservedOp "!="
+        return Neq
+
+pLeqOp
+    = do
+        reservedOp "<="
+        return Leq
+
+pGeqOp
+    = do
+        reservedOp ">="
+        return Geq
+
+pGreOp
+    = do
+        reservedOp ">"
+        return Gre
+
+pLesOp
+    = do
+        reservedOp "<"
+        return Les
+
+pTerm 
+  = chainl1 pFactor pMulOp
+    <?>
+    "\"term\""
+
+pMulOp
+  = do 
+      reservedOp "*"
+      return Mul
+
+pFactor
+  = choice [pUminus, parens pExp, pNum, pIdent]
+    <?> 
+    "\"factor\""
+
+pUminus
+  = do 
+      reservedOp "-"
+      exp <- pFactor
+      return (UnaryMinus exp)
+
+pNum
+  = do
+      n <- natural <?> ""
+      return (IntConst (fromInteger n :: Int))
+    <?>
+    "number"
+
+pIdent 
+  = do
+      ident <- identifier
+      return (Id ident)
+    <?>
+    "identifier"
+
+pLvalue :: Parser Lvalue
+pLvalue
+  = do
+      ident <- identifier
+      return (LId ident)
+    <?>
+    "lvalue"
+
+-----------------------------------------------------------------
+-- main
+-----------------------------------------------------------------
+
+pMain :: Parser KidProgram
+pMain
+  = do
+      whiteSpace
+      p <- pProg
+      eof
+      return p
+
+main :: IO ()
+main
+  = do { progname <- getProgName
+       ; args <- getArgs
+       ; checkArgs progname args
+       ; input <- readFile (head args)
+       ; let output = runParser pMain 0 "" input
+       ; case output of
+           Right ast -> print ast
+           Left  err -> do { putStr "Parse error at "
+                           ; print err
+                           }
+       }
+
+checkArgs :: String -> [String] -> IO ()
+checkArgs _ [filename]
+   = return ()
+checkArgs progname _
+   = do { putStrLn ("Usage: " ++ progname ++ " filename\n\n")
+       ; exitWith (ExitFailure 1)
+       }
